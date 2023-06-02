a81e1e90de25da1106e3b29d5886806475ec9299
apps4sj <apps4sj@gmail.com>
2023-06-02 09:31:11 -0700

    #Feature Update logo icon and add opencv license splash page

---
+++ app/src/main/java/com/example/dualphoneshooter/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.dualphoneshooter;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    // Set the duration of the splash screen in milliseconds
+    private static final int SPLASH_DURATION = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        // Delay execution and start MainActivity after the splash duration
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish(); // Optional: Close the splash activity to prevent going back to it
+            }
+        }, SPLASH_DURATION);
+    }
+}
+
